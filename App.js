595e7b4340c08a41c7ab27491dc317f01b6f06d7
Andrea Borg <andreaborg217@gmail.com>
2022-09-20 17:40:29 +0200

    initial project & boilerplate

---
+++ App.js
@@ -0,0 +1,31 @@
+/* eslint-disable prettier/prettier */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   width: '100%',
+   height: '100%',
+   backgroundColor: '#845EC2',
+   alignItems: 'center',
+   justifyContent: 'center',
+  },
+});
+
+export default App;
