c4008087109c1f8fcb1134231e4d8a8c62459a73
Andrea Borg <andreaborg217@gmail.com>
2022-09-20 18:02:01 +0200

    Finished navbar layout and styling

--- App.js
+++ App.js
@@ -7,12 +7,29 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 
 const App = () => {
+  const [widget, setWidget] = useState('stopwatch');
+
   return (
     <View style={styles.container}>
+
+    <View style = {styles.navbar}>
+      <TouchableOpacity style = {[styles.navbutton, {backgroundColor: widget == 'stopwatch'? '#ff6F91': '#D65DB1'}]} onPress = {() => setWidget('stopwatch')}>
+        <Image style={styles.icon} source={require('./assets/stopwatch.png')}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity style = {[styles.navbutton, {backgroundColor: widget == 'timer'? '#ff6F91': '#D65DB1'}]} onPress = {() => setWidget('timer')}>
+        <Image style={styles.icon} source={require('./assets/timer.png')}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity style = {[styles.navbutton, {backgroundColor: widget == 'alarm'? '#ff6F91': '#D65DB1'}]} onPress = {() => setWidget('alarm')}>
+        <Image style={styles.icon} source={require('./assets/alarm.png')}/>
+      </TouchableOpacity>
+    </View>
+    
     </View>
   );
 };
@@ -22,10 +39,27 @@ const styles = StyleSheet.create({
    flex: 1,
    width: '100%',
    height: '100%',
-   backgroundColor: '#845EC2',
+   backgroundColor: '#9883CB',
    alignItems: 'center',
    justifyContent: 'center',
   },
+  navbar:{
+    flexDirection: 'row',
+    position: 'absolute',
+    transform: [{translateY: -300}]
+  },
+  navbutton:{
+    width: 110,
+    height: 54,
+    margin: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30
+  },
+  icon:{
+    width: 32, 
+    height: 32
+  }
 });
 
 export default App;
