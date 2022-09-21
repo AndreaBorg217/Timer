27eb1c7247d73b3b148ffe2f1f6b5bc858fb8dc0
Andrea Borg <andreaborg217@gmail.com>
2022-09-21 09:21:19 +0200

    added play, pause and stop buttons to timer widget

--- widgets/timer.js
+++ widgets/timer.js
@@ -11,11 +11,29 @@
  import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
  
  const Timer = () => {
-   const [widget, setWidget] = useState('stopwatch');
- 
+ const [timer, setTimer] = useState('00:00:00')
+ const [playing, setPlaying] = useState(false)
    return (
      <View style={styles.container}>
-        <Text style = {styles.text}>TIMER</Text>
+        <Text style = {styles.text}>{timer}</Text>
+
+     <View style = {styles.buttonContainer}>
+          {!playing?(
+            <TouchableOpacity style = {styles.button} onPress = {() => {setPlaying(true)}}>
+              <Image style={styles.icon} source={require('../assets/play.png')}/>
+            </TouchableOpacity>
+          ): 
+          <View style = {styles.twoButtons}>
+            <TouchableOpacity style = {[styles.button, {marginRight: 150}]} onPress = {() => {setPlaying(false)}}>
+              <Image style={styles.icon} source={require('../assets/stop.png')}/>
+            </TouchableOpacity>
+
+            <TouchableOpacity  style = {[styles.button, {transform: [{translateX: 10}]}]} onPress = {() => {setPlaying(false)}}>
+              <Image style={styles.icon} source={require('../assets/pause.png')}/>
+            </TouchableOpacity>
+          </View>
+        }
+      </View>
      </View>
    );
  };
@@ -24,10 +42,30 @@
     container: {
         width: 300,
         height: 300,
+        alignItems: 'center',
     },
     text:{
         color: 'white',
-        fontSize: 50
+        fontSize: 70
+    },
+    button:{
+      width: 70,
+      height: 70,
+      borderRadius: 70/2,
+      backgroundColor: '#00AAB2',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    icon:{
+      width: 32, 
+      height: 32
+    },
+    buttonContainer:{
+      position: 'absolute',
+      transform: [{translateY: 350}]
+    },
+    twoButtons:{
+      flexDirection: 'row',
     }
  });
  
