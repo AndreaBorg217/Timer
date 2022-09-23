3819af7a9baf5110083a21ee85b7d7f0e36cbd4d
Andrea Borg <andreaborg217@gmail.com>
2022-09-23 10:58:10 +0200

    added clock picker and add alarm button

--- widgets/alarm.js
+++ widgets/alarm.js
@@ -9,13 +9,40 @@
 
  import React, {useState} from 'react';
  import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
- 
+ import RNDateTimePicker from '@react-native-community/datetimepicker';
+
+
  const Alarm = () => {
-   const [widget, setWidget] = useState('stopwatch');
- 
-   return (
+  const [pickerVisible, setPickerVisible] = useState(false); 
+  const [alarms, setAlarms] = useState([])
+  
+  return (
      <View style={styles.container}>
-        <Text style = {styles.text}>ALARM</Text>
+        {pickerVisible?(
+            <RNDateTimePicker 
+              mode="time" 
+              display="clock" 
+              value={new Date()} 
+              is24Hour={true}
+              positiveButtonLabel="Add alarm"  
+              onChange={(event, time)=>{
+                if(event.type == 'Cancel'){
+                  setPickerVisible(false)
+                }
+                else if(event.type == 'Add alarm'){
+                  if(alarms && !alarms.includes(time)){
+                    setAlarms(prev => prev.push(time))
+                  }
+                  setPickerVisible(false)
+                }
+              }}
+            />
+        ): null}
+
+
+      <TouchableOpacity style = {styles.button} onPress = {() => setPickerVisible(true)}>
+        <Image style={styles.icon} source={require('../assets/plus.png')}/>
+      </TouchableOpacity>        
      </View>
    );
  };
@@ -24,10 +51,27 @@
     container: {
         width: 300,
         height: 300,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-    text:{
-        color: 'white',
-        fontSize: 50
+    button:{
+      width: 70,
+      height: 70,
+      borderRadius: 70/2,
+      backgroundColor: '#00AAB2',
+      alignItems: 'center',
+      justifyContent: 'center',
+      transform: [{translateY: 235}]
+    },
+    icon:{
+      width: 32, 
+      height: 32
+    },
+    test:{
+      backgroundColor: 'red',
+      width: 300,
+      height: 300,
+      position: 'absolute'
     }
  });
  
