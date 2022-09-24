b8237bb3d86427c89174d5c44bd4edffcd446d10
Andrea Borg <andreaborg217@gmail.com>
2022-09-24 10:57:52 +0200

    added stopwatch indicator with formatting function

--- widgets/stopwatch.js
+++ widgets/stopwatch.js
@@ -10,25 +10,49 @@
  import React, {useState} from 'react';
  import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
  
+ const formatTime = (minutes, seconds, milliseconds) =>{
+  if(minutes < 10 && minutes >= 0){
+    minutes = '0' + minutes;
+  }
+  if(seconds < 10){
+    seconds = '0' + seconds;
+  }
+  if(milliseconds < 10){
+    milliseconds = '00' + milliseconds;
+  }
+  if(milliseconds < 100 && milliseconds > 10){
+    milliseconds = '0' + milliseconds;
+  }
+  return minutes + ':' + seconds + ':' + milliseconds;
+ }
+
+
  const Stopwatch = () => {
-   const [widget, setWidget] = useState('stopwatch');
- 
+  const [minutes, setMinutes] = useState(0)
+  const [seconds, setSeconds] = useState(0)
+  const [ms, setMs] = useState(0)
    return (
      <View style={styles.container}>
-        <Text style = {styles.text}>STOPWATCH</Text>
+        <Text style = {styles.stopwatch}>{formatTime(minutes, seconds, ms)}</Text>
      </View>
    );
  };
  
  const styles = StyleSheet.create({
     container: {
-        width: 300,
-        height: 300,
+      width: 300,
+      height: 300,
+      alignItems: 'center',
+      justifyContent: 'center',
     },
     text:{
-        color: 'white',
-        fontSize: 50
-    }
+      color: 'white',
+      fontSize: 50
+    },
+    stopwatch:{
+      color: 'white',
+      fontSize: 65,
+    },
  });
  
  export default Stopwatch;
