24dd840e34517b9a4d63a95b5ec1e7f10fa3ea25
Andrea Borg <andreaborg217@gmail.com>
2022-09-24 11:20:10 +0200

    finished button transition in accordance to playing & paused states

--- widgets/stopwatch.js
+++ widgets/stopwatch.js
@@ -31,9 +31,46 @@
   const [minutes, setMinutes] = useState(0)
   const [seconds, setSeconds] = useState(0)
   const [ms, setMs] = useState(0)
+  const [playing, setPlaying] = useState(false)
+  const [paused, setPaused] = useState(false)
+
    return (
      <View style={styles.container}>
         <Text style = {styles.stopwatch}>{formatTime(minutes, seconds, ms)}</Text>
+
+        <View style = {styles.buttonContainer}>
+        {!playing && !paused?(
+            <TouchableOpacity style = {styles.button} onPress = {() => setPlaying(true)}>
+              <Image style={styles.icon} source={require('../assets/play.png')}/>
+            </TouchableOpacity>        
+          ):
+          <View style = {styles.row}>
+          {playing && !paused?(
+            <View style={styles.row}>
+              <TouchableOpacity style = {[styles.button, {marginRight: 160}]} onPress = {()=>{}}>
+                <Image style={styles.icon} source={require('../assets/lap.png')}/>
+              </TouchableOpacity>
+              <TouchableOpacity style = {styles.button} onPress = {() => {setPlaying(false); setPaused(true)}}>
+                <Image style={styles.icon} source={require('../assets/pause.png')}/>
+              </TouchableOpacity>   
+            </View>      
+          ): 
+            <View style = {styles.row}>
+            {!playing && paused?(
+            <View style={styles.row}>
+              <TouchableOpacity style = {[styles.button, {marginRight: 160}]} onPress = {() => {setPlaying(true); setPaused(false)}}>
+                <Image style={styles.icon} source={require('../assets/play.png')}/>
+              </TouchableOpacity>   
+              <TouchableOpacity style = {styles.button} onPress = {()=>{setPlaying(false); setPaused(false); setMinutes(0); setSeconds(0); setMs(0)}}>
+                <Image style={styles.icon} source={require('../assets/stop.png')}/>
+              </TouchableOpacity>
+            </View>  
+            ):null}
+            </View>
+          }
+          </View>
+        }
+      </View>
      </View>
    );
  };
@@ -43,15 +80,30 @@
       width: 300,
       height: 300,
       alignItems: 'center',
-      justifyContent: 'center',
-    },
-    text:{
-      color: 'white',
-      fontSize: 50
     },
     stopwatch:{
       color: 'white',
       fontSize: 65,
+      position: 'absolute',
+    },
+    button:{
+      width: 70,
+      height: 70,
+      borderRadius: 70/2,
+      backgroundColor: '#00AAB2',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    icon:{
+      width: 32, 
+      height: 32
+    },
+    buttonContainer:{
+      position: 'absolute',
+      transform: [{translateY: 350}]
+    },
+    row:{
+      flexDirection: 'row',
     },
  });
  
